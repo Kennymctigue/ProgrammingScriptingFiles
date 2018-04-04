55a98818acf9e25154140ea1e027441e9bb1b724
Kennymctigue <36159017+Kennymctigue@users.noreply.github.com>
2018-04-04 21:07:41 +0100

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,13 +1,14 @@
-# kenny mctigue 08/02/2018
+# kenny mctigue, 08/02/2018
 # collatz conjecture task wk3
+# https://study.com/academy/lesson/history-of-the-collatz-conjecture.html
+# https://stackoverflow.com/questions/13366830/collatz-conjecture-sequence
 
-n = int(input("Enter any integer number:"))
+n = int(input("Enter any integer number:")) #Allows the user of the program to enter the integer they choose
 
 
-while n > 1:
-    if (n % 2 == 0):
-        n = n / 2
+while n > 1: #continue loop until the input number is reduced to 1
+    if (n % 2 == 0): # if the integer can be dived by two evenly
+        n = n / 2 #divide the number by 2
     else:
-        n = (n * 3)+1
-    print (n)
-
+        n = (n * 3)+1 #multiply the number by 3 and add 1
+    print (n) #print the eventual value of n
